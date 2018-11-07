f3a006985fc513e526e7dfc11be5adde1f08f129
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-07 00:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function distanceFromHqInFeet(distance) {
 function distanceTravelledInFeet (start, destination) {
     return |destination - start| * 264;
   }
+
+  function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
