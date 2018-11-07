613d5a5848cf0717fd266d9ee543aa5c880f0744
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-07 00:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(distance) {
-  return |distance - 42|;
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
 }
 
-function distanceFromHqInFeet(distance) {
-  return distanceFromHqInBlocks(distance) * 264;
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
 function distanceTravelledInFeet (start, destination) {
-    return |destination - start| * 264;
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
   }
+}
 
-  function calculatesFarePrice (start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
 
-    if (distance <= 400) {
-      return 0;
-    } else if (distance > 400 && distance <= 2000) {
-      return .02 * (distance - 400);
-    } else if (distance > 2000 && distance < 2500) {
-      return 25;
-    } else {
-      return 'cannot travel that far';
-    }
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
+}
